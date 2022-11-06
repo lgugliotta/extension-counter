510a903bc6eefe485f3c0e8127c8dd2164496fff
Leandro Gugliotta <lgugliotta@gmail.com>
2022-11-06 18:43:16 +0100

    remove unnecesary

--- tests/controllers/github.controller.test.js
+++ tests/controllers/github.controller.test.js
@@ -1,6 +1,5 @@
 const githubController = require('../../controllers/github.controller');
 const gitHubService = require("../../services/github.service");
-const logger = require('../../utils/logger');
 const treeMock = require("../mocks/tree.json");
 const treeWithoutFolders = require('../mocks/tree-without-folders.json')
 
@@ -10,12 +9,7 @@ describe('Github controller tests', () => {
   };
   const mocks = {
     getTreeMock: gitHubService.getTree = jest.fn(),
-    logMock: logger.log = jest.fn()
   }
-
-  beforeEach(() => {
-    Object.keys(mocks).map((e) => mocks[e].mockReset());
-  })
 
   /**
    * This emulates a repo with 3 folders in root and each folder have 2 files
@@ -29,8 +23,4 @@ describe('Github controller tests', () => {
     const result = await githubController.getExtensionCounter(body);
     expect(result).toHaveProperty('js')
   })
-
-  it('should return error for invalid body', function () {
-
-  });
 })
